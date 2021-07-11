67eda1ae2740bc4ccd7c913136115a4cec0f629d
SanketTambe39 <sankettambe39@gmail.com>
2021-07-12 00:00:50 +0530

    ADDED: Domain name validation

--- EmailValidation/src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ EmailValidation/src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -7,7 +7,18 @@ public class EmailValidation {
 	public static void main(String[] args) {
 		String string = "sanket";
 		checkFirstLetter(string);
-		
+		String str2 = "@bridgelabz";
+		checkDomainName(str2);
+	}
+	private static void checkDomainName(String str2) {
+
+		boolean isMatched = Pattern.compile("^@[a-zA-Z]+").matcher(str2).matches();
+		if (isMatched) {
+			System.out.println("Domain name is Perfect");
+		} else {
+			System.out.println("Domain name is not Perfect");
+		}
+
 	}
 	private static void checkFirstLetter(String string) {
 		boolean isMatched = Pattern.compile("^[0-9a-zA-Z]+").matcher(string).matches();
