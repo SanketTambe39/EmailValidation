f452feaec896c04ce095fd575e795a219fa3fdee
SanketTambe39 <sankettambe39@gmail.com>
2021-07-11 23:59:02 +0530

    ADDED: first letter validation

--- EmailValidation/src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ EmailValidation/src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -1,10 +1,20 @@
 package com.bridgelabz.emailvalidation;
 
+import java.util.regex.Pattern;
+
 public class EmailValidation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String string = "sanket";
+		checkFirstLetter(string);
+		
 	}
-
+	private static void checkFirstLetter(String string) {
+		boolean isMatched = Pattern.compile("^[0-9a-zA-Z]+").matcher(string).matches();
+		if (isMatched) {
+			System.out.println("Correct Till Now");
+		} else {
+			System.out.println("Something went wrong");
+		}
+	}
 }
